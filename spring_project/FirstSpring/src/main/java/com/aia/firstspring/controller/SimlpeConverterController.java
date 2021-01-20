8d2d5c3693cef8bc44e999ea47c5fcd5e049350f
2hundred2ne2 <2hundred2ne2@gmail.com>
2021-01-20 13:01:11 +0900

    210120

---
+++ spring_project/FirstSpring/src/main/java/com/aia/firstspring/controller/SimlpeConverterController.java
@@ -0,0 +1,27 @@
+package com.aia.firstspring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/mc/simple")
+public class SimlpeConverterController {
+	
+	@RequestMapping(method = RequestMethod.GET)
+	public String form() {
+	
+		
+		return "simple/form";
+	}
+	@RequestMapping(method = RequestMethod.POST)
+	@ResponseBody
+	public String simple(@RequestBody String body) {
+		System.out.println(body+"@ResponseBody 어노테이션은 일반 문자열 처리가 됩니다");
+		return body+"@ResponseBody 어노테이션은 일반 문자열 처리가 됩니다";
+	}
+	
+
+}
